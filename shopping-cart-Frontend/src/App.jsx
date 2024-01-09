4ff3d87169e38e47c7176550dde697e01be26393
Gustaf Ekström <gusek93@gmail.com>
2024-01-09 21:16:55 +0100

    Added display page for cart. Delete itemok

--- shopping-cart-Frontend/src/App.jsx
+++ shopping-cart-Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import axios from "axios";
 import ProductList from './components/ProductList';
+import ShoppingCartContent from './components/ShoppingCartContent';
 import "./App.css"
 import 'bootstrap/dist/css/bootstrap.min.css';
 import ShoppingCart from './components/ShoppingCart';
@@ -10,7 +11,8 @@ const App = () => {
 
   const [myProducts, setMyProducts] = useState([]);
   const [shoppingCartItems, setShoppingCartItems] = useState([]);
-
+  const [showCartContent, setShowCartContent] = useState(false);
+  
   const stateVariables = {shoppingCartItems, setShoppingCartItems};
   useEffect(()=>{
     const loadProduct = async () => {
@@ -21,9 +23,32 @@ const App = () => {
 
   }, []);
 
+  const toggleShowContent = () => {
+    setShowCartContent(!showCartContent);
+  }
+
+  const removeItem = (product) => {
+    
+    let found = false;
+    const fixedItems = shoppingCartItems.filter((item) => {
+      if(!found && item.name === product.name) {
+        found = true;
+        return false;
+      }
+      else {
+        return true;
+      }
+    });
+    setShoppingCartItems(fixedItems);
+  }
+
   return (
     <div className='app'>
-      <ShoppingCart shoppingCartItems={shoppingCartItems}/>
+      <div className={`overlay ${showCartContent ? 'active' : ""}`}></div>
+      <div className='shopping-cart-icon' onClick={toggleShowContent}>
+        <ShoppingCart shoppingCartItems={shoppingCartItems}/>  
+      </div>
+      {showCartContent && <ShoppingCartContent shoppingCartItems={shoppingCartItems} removeItem={removeItem}/>}          
       <span className='shopping-cart-counter'>{shoppingCartItems.length}</span>
       <h2 className='app-headline'>Shopping Cart App</h2>
       {myProducts.length > 0 ? (
@@ -31,8 +56,6 @@ const App = () => {
       ):
         <p>No products available</p>
       }
-
-      <button className='test-button' onClick={()=> console.log(shoppingCartItems)}>Check Shopping Cart</button>
       
 
     </div>
---
+++ shopping-cart-Frontend/src/components/ShoppingCartContent.jsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from 'react'
+import "./shoppingCartContent.css"
+
+const ShoppingCartContent = ({shoppingCartItems, removeItem}) => {
+  
+  
+
+  const shoppingCartItemsCalc = () => {
+    const calculatedItems = [];
+    shoppingCartItems.forEach(item => {
+      if(calculatedItems[item.name]){
+        calculatedItems[item.name].quantity += 1;
+      }
+      else {
+        calculatedItems[item.name] = {
+          ...item,
+          quantity: 1
+        }
+      }
+    });
+    return Object.values(calculatedItems);
+  }
+  
+
+  const calculatedItemsArray = shoppingCartItemsCalc(shoppingCartItems);
+  const totalPrice = calculatedItemsArray.reduce((sum, item) => sum + (item.quantity * item.price), 0);
+  return (
+    <div className='shopping-cart-content'>
+      <div className='inner-frame'>
+        <h4>Shopping Cart</h4>
+        <ul className='cart-items'>
+          {calculatedItemsArray.map((item, index) =>(
+            <li className='product-info' key={index}>
+              <span className='name'>{item.name}</span>
+              <span className='quantity'>{`Amount: ${item.quantity}`}</span>
+              <span className='price'>{`Price: ${item.price * item.quantity} $`}</span>
+              <div className='close-button' onClick={()=>removeItem(item)}>&times;</div>
+            </li>
+          ))}
+          {shoppingCartItems.length === 0 && <p>Your cart is empty</p>}
+        </ul>
+        <span className='total-price'>{`Total Price: ${totalPrice} $`}</span>
+      </div>
+
+      <button className='checkout'>Proceed to Checkout</button>
+      
+    </div>
+  );
+};
+
+export default ShoppingCartContent
